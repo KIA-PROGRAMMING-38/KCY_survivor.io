46e69121854947ecde500c3074c26890570f9718
0105KCY <ggbbsing@gmail.com>
2023-04-05 19:31:46 +0900

    [Feat] Camera Move

---
+++ survivor.io/Assets/Scripts/TrackCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCamera : MonoBehaviour
+{
+    public GameObject target;
+    
+   
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -1);
+    }
+}
