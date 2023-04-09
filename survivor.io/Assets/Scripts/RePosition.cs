c24fef94e389fec8e8d30b99592d83f6f8f876dc
0105KCY <ggbbsing@gmail.com>
2023-04-10 00:47:28 +0900

    [Feat] Monster Reposition

--- survivor.io/Assets/Scripts/RePosition.cs
+++ survivor.io/Assets/Scripts/RePosition.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class RePosition : MonoBehaviour
@@ -14,16 +15,34 @@ public class RePosition : MonoBehaviour
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 panelPos = GetComponent<Transform>().position;
         float panelDir = playerPos.y - panelPos.y;
-        Vector3 movePos = new Vector3(0f, 340, 0f);
+        Vector3 movePos = new Vector3(0f, 340f, 0f);
+        Vector3 moveMonster = new Vector3(0f, 230f, 0f);
 
-       
-        if (panelDir > 0)
+        switch (transform.tag)
         {
-            GetComponent<Transform>().position += movePos;
-        }
-        else
-        {
-            GetComponent<Transform>().position -= movePos;
+            case "Ground":
+
+                if (panelDir > 0)
+                {
+                    GetComponent<Transform>().position += movePos;
+                }
+                else
+                {
+                    GetComponent<Transform>().position -= movePos;
+                }
+                break;
+
+            case "Monster":
+
+                if (panelDir > 0)
+                {
+                    GetComponent<Transform>().position += moveMonster;
+                }
+                else
+                {
+                    GetComponent<Transform>().position -= moveMonster;
+                }
+                break;
         }
     }
 }
