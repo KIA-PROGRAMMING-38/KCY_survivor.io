28fb11734a1478d1671ddce80fc72c9cdad773dc
0105KCY <ggbbsing@gmail.com>
2023-04-10 00:27:17 +0900

    [Feat] Trace Target, Flip Sprite

---
+++ survivor.io/Assets/Scripts/TraceTarget.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceTarget : MonoBehaviour
+{
+    public Rigidbody2D target;
+    private Rigidbody2D rigid;
+    private SpriteRenderer renderer;
+    public float speed;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector2 tracePos = target.transform.position - transform.position;
+        rigid.MovePosition(rigid.position + tracePos.normalized * speed * Time.fixedDeltaTime);
+        rigid.velocity = Vector2.zero;
+
+    }
+    private void LateUpdate()
+    {
+        if (target.transform.position.x - transform.position.x < 0)
+        {
+            renderer.flipX = true;
+        }
+        else
+        {
+            renderer.flipX = false;
+        }
+    }
+}
