5c7ffbef24b3bd697a2ccffc33e236efe9472ff6
0105KCY <ggbbsing@gmail.com>
2023-04-09 22:27:56 +0900

    [Style] Clear Line

--- survivor.io/Assets/Scripts/PlayerMove.cs
+++ survivor.io/Assets/Scripts/PlayerMove.cs
@@ -8,15 +8,12 @@ public class PlayerMove : MonoBehaviour
     private float InputY;
     public float _speed;
     private Rigidbody2D playerBody;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
         playerBody = GetComponent<Rigidbody2D>();
-       
     }
    
-    // Update is called once per frame
     void Update()
     {
         InputX = Input.GetAxis("Horizontal");
--- survivor.io/Assets/Scripts/TrackCamera.cs
+++ survivor.io/Assets/Scripts/TrackCamera.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TrackCamera : MonoBehaviour
 {
     public GameObject target;
-    
    
     private void LateUpdate()
     {
