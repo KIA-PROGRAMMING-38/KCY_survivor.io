3af63018979a4440ed834241e00cd820790ee5db
0105kcy <120005245+0105kcy@users.noreply.github.com>
2023-04-25 02:18:50 +0900

    28 feature 무기 레벨 업 UI 이름 추가 (#37)
    * [Feat] Add WeaponNameUi
    
    * [Feat] Weapon Name Ui

--- survivor.io/Assets/Scripts/UI/SelectWeapon.cs
+++ survivor.io/Assets/Scripts/UI/SelectWeapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class SelectWeapon : MonoBehaviour
 {
     public AbleWeapons ableWeaponsData; // 사용가능한 무기 데이터
     private GameObject imageBox; // 무기이미지 공간
+    private TMP_Text weaponName;
     private GameObject saveWeapon;
     private Image weaponImage;
     private int weaponIndex;
@@ -15,6 +17,7 @@ public class SelectWeapon : MonoBehaviour
     {
         imageBox = transform.Find("WeaponImage").gameObject;
         weaponImage = imageBox.GetComponent<Image>();
+        weaponName = transform.Find("WeaponName").GetComponent<TMP_Text>();
     }
 
  
@@ -23,7 +26,9 @@ public class SelectWeapon : MonoBehaviour
         weaponIndex = Random.Range(0, ableWeaponsData.ableWeapons.Count - 1);
         saveWeapon = ableWeaponsData.ableWeapons[weaponIndex];
         weaponImage.sprite = ableWeaponsData.ableWeapons[weaponIndex].GetComponent<SpriteRenderer>().sprite;
+        weaponName.text = saveWeapon.name;
         ableWeaponsData.ableWeapons.Remove(saveWeapon);
+        
     }
 
     private void OnDisable()
