4b54bb2696f462c71bb01e6b13465350199deab0
0105KCY <ggbbsing@gmail.com>
2023-04-25 03:14:34 +0900

    [Feat] Add WeaponText

--- survivor.io/Assets/Scripts/UI/SelectWeapon.cs
+++ survivor.io/Assets/Scripts/UI/SelectWeapon.cs
@@ -7,26 +7,44 @@ using UnityEngine.UI;
 public class SelectWeapon : MonoBehaviour
 {
     public AbleWeapons ableWeaponsData; // 사용가능한 무기 데이터
-    private GameObject imageBox; // 무기이미지 공간
     private TMP_Text weaponName;
+    private TMP_Text weaponLevelText;
+    private WeaponText weaponText;
     private GameObject saveWeapon;
     private Image weaponImage;
     private int weaponIndex;
+    private int firstText;
     
     private void Awake()
     {
-        imageBox = transform.Find("WeaponImage").gameObject;
-        weaponImage = imageBox.GetComponent<Image>();
+
+        weaponImage = transform.Find("WeaponImage").gameObject.GetComponent<Image>();
         weaponName = transform.Find("WeaponName").GetComponent<TMP_Text>();
+        weaponLevelText = transform.Find("LevelUpText").GetComponent<TMP_Text>();
+
     }
 
- 
+    private void Start()
+    {
+        firstText = 0;
+    }
+
+
     private void OnEnable()
     {
         weaponIndex = Random.Range(0, ableWeaponsData.ableWeapons.Count - 1);
         saveWeapon = ableWeaponsData.ableWeapons[weaponIndex];
         weaponImage.sprite = ableWeaponsData.ableWeapons[weaponIndex].GetComponent<SpriteRenderer>().sprite;
         weaponName.text = saveWeapon.name;
+        weaponText = saveWeapon.GetComponent<WeaponText>();
+        if (WeaponManager.Instance.weapons.ContainsKey(saveWeapon.name))
+        {
+            weaponLevelText.text = weaponText.weaponText.levelupText[weaponText.weaponText.Level];
+        }
+        else
+        {
+            weaponLevelText.text = weaponText.weaponText.levelupText[firstText];
+        }
         ableWeaponsData.ableWeapons.Remove(saveWeapon);
     }
 
